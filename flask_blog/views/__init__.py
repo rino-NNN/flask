f6a470df15a6982a7a39f105a2d6c73baa421375
RinoM <peach.rin.2002.01.27@gmail.com>
2022-08-12 14:43:38 +0900

    25途中まで

--- flask_blog/views/__init__.py
+++ flask_blog/views/__init__.py
@@ -4,6 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('flask_blog.config')
 
-db = SQLAlachemy(app)
+db = SQLAlchemy(app)
 
-import flask_blog.views
+from flask_blog.views.views import views, entries
